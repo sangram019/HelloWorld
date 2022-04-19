94fc5d4c7eb8d5585c745affb500c546a52ac364
Sangram <sangram15.behera@gmail.com>
2022-04-20 02:48:14 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi This Is The First Program");
+        }
+    }
+}
+
